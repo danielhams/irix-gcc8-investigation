52b636a41b228d8850a5798c67ec3c2e06d30107
Daniel Hams <daniel.hams@gmail.com>
2019-06-23 00:34:53 +0100

    Generate some output that can be compared. Patches to gcc4 and gcc8 will be added.

--- dotests.sh
+++ dotests.sh
@@ -9,23 +9,23 @@ export GCC8_INSTALL_ROOT=$MBROOT/gbs8_test
 
 # (1) Build executables with gcc4, gcc8
 
-./buildexe.sh
+#./buildexe.sh
 
 export ORIG_LLP=$LD_LIBRARYN32_PATH
 
 # (2) Run against the gcc4 libs, obtain some trace
-export LD_LIBRARYN32_PATH=$GCC4_INSTALL_ROOT/lib:$ORIG_LLP
+export LD_LIBRARYN32_PATH=$GCC4_INSTALL_ROOT/lib32:$ORIG_LLP
 echo "------ GCC4 COMPILER, GCC4 LIBS RUN ------"
-./throw_test_4.exe
+./throw_test_4.exe | tee comp4libs4.out
 
 echo "------ GCC8 COMPILER, GCC4 LIBS RUN ------"
-./throw_test_8.exe
+./throw_test_8.exe |tee comp8libs4.out
 
 # (3) Run against the gcc8 libs, obtain same trace
-export LD_LIBRARYN32_PATH=$GCC8_INSTALL_ROOT/lib:$ORIG_LLP
+export LD_LIBRARYN32_PATH=$GCC8_INSTALL_ROOT/lib32:$ORIG_LLP
 echo "------ GCC4 COMPILER, GCC8 LIBS RUN ------"
-./throw_test_4.exe
+./throw_test_4.exe |tee comp4libs8.out
 
-export LD_LIBRARYN32_PATH=$GCC8_INSTALL_ROOT/lib:$ORIG_LLP
+export LD_LIBRARYN32_PATH=$GCC8_INSTALL_ROOT/lib32:$ORIG_LLP
 echo "------ GCC8 COMPILER, GCC8 LIBS RUN ------"
-./throw_test_8.exe
+./throw_test_8.exe |tee comp8libs8.out
